7131338a10b8a458518f877a9b5f601bf82a340a
sapna sahai <47326414+sapnasahai@users.noreply.github.com>
2025-12-28 13:53:30 +0530

    package remove that deprecated

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Reference variables
+    private Camera mainCamera;
+
+    
+
+    public float moveSpeed = 5f;
+
+    // 🔹 Awake is called FIRST
+    void Awake()
+    {
+        // Reference assignment (setup work)
+         mainCamera = Camera.main;
+
+        Debug.Log("Awake: Camera reference assigned");
+    }
+
+    // 🔹 Start is called AFTER all Awake methods
+    void Start()
+    {
+        // Gameplay logic
+        Debug.Log("Start: Player is ready to move");
+    }
+
+    void Update()
+    {
+        
+            float horizontal = Input.GetAxis("Horizontal"); // A / D
+
+            transform.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
+       
+    }
+}
